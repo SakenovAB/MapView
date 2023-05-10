5060f0c2d29d7df54c699aad4821de66061794d0
Azikeaza <95418570+Azikeaza@users.noreply.github.com>
2023-05-10 15:58:21 +0600

    First Commit

---
+++ mapView/ViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ViewController.swift
+//  mapView
+//
+//  Created by Азамат Сакенов on 10.05.2023.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate, UIGestureRecognizerDelegate {
+    
+    @IBOutlet weak var mapview: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    var userLocation = CLLocation()
+    
+    var followMe = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Запрашиваем разрешение на использование местоположения пользователя
+        locationManager.requestWhenInUseAuthorization()
+        
+        // delegate нужен для функции didUpdateLocations, которая вызывается при обновлении местоположения (для этого прописали CLLocationManagerDelegate выше)
+        locationManager.delegate = self
+        
+        // Запускаем слежку за пользователем
+        locationManager.startUpdatingLocation()
+        
+        // Настраиваем отслеживания жестов - когда двигается карта вызывается didDragMap
+        let mapDragRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.didDragMap))
+        
+        // UIGestureRecognizerDelegate - чтоб мы могли слушать нажатия пользователя по экрану и отслеживать конкретные жесты
+        mapDragRecognizer.delegate = self
+        
+        // Добавляем наши настройки жестов на карту
+        mapview.addGestureRecognizer(mapDragRecognizer)
+        
+        // ______________ Метка на карте ______________
+        // Новые координаты для метки на карте
+        let lat:CLLocationDegrees = 37.957666//43.2374454
+        let long:CLLocationDegrees = -122.0323133//76.909891
+        
+        // Создаем координта передавая долготу и широту
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(lat, long)
+        
+        // Создаем метку на карте
+        let anotation = MKPointAnnotation()
+        
+        // Задаем коортинаты метке
+        anotation.coordinate = location
+        // Задаем название метке
+        anotation.title = "Title"
+        // Задаем описание метке
+        anotation.subtitle = "subtitle"
+        
+        // Добавляем метку на карту
+        mapview.addAnnotation(anotation)
+        // ______________ Метка на карте ______________
+    }
+    // Вызывается каждый раз при изменении местоположения нашего пользователя
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // Координаты пользователя
+        userLocation = locations[0]
+        
+        // Проверяем нужно ли следовать за пользователем
+        if followMe {
+            // Дельта - насколько отдалиться от координат пользователя по долготе и широте
+            let latDelta:CLLocationDegrees = 0.01
+            let longDelta:CLLocationDegrees = 0.01
+            
+            // Создаем область шириной и высотой по дельте
+            let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+            
+            // Создаем регион на карте с моими координатоми в центре
+            let region = MKCoordinateRegion(center: userLocation.coordinate, span: span)
+            
+            // Приближаем карту с анимацией в данный регион
+            mapview.setRegion(region, animated: true)
+        }
+    }
+    
+    @IBAction func ShowMyLocation(_ sender: Any) {
+        followMe = true
+    }
+    // Вызывается когда двигаем карту
+    @objc func didDragMap(gestureRecognizer: UIGestureRecognizer) {
+        // Как только начали двигать карту
+        if (gestureRecognizer.state == UIGestureRecognizer.State.began) {
+            
+            // Говорим не следовать за пользователем
+            followMe = false
+            
+            print("Map drag began")
+        }
+        
+        // Когда закончили двигать карту
+        if (gestureRecognizer.state == UIGestureRecognizer.State.ended) {
+            
+            print("Map drag ended")
+        }
+    }
+    
+}
